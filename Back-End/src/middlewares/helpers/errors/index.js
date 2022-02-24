69068493a7aa50f10b188cbbdb8c6027a3feb398
Garcia Mata Miguel <miguelgmata@macbook-air-de-garcia.home>
2022-02-24 12:04:27 +0100

    modification de middlewares avec l'utilisation de helpers

---
+++ Back-End/src/middlewares/helpers/errors/index.js
@@ -0,0 +1,12 @@
+const BadRequestError = require('./bad_request_error');
+const ServerError = require('./server_error');
+const ConflictError = require('./conflict_error');
+const ForbiddenError = require('./forbidden_error');
+const UnauthorizedError = require('./unauthorized_error');
+const NotFoundError = require('./not_found_error');
+
+module.exports = {
+  ServerError,
+  UnauthorizedError,
+  NotFoundError
+};
